19da7fc83795bf19c3659c05307f191845351abb
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-08-01 16:33:28 +0530

    done

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -1,41 +1,66 @@
+import { useState } from "react"
 
 const MyForm = () => {
 
+    const [studName, setstudName] = useState("")
+    const [studEmail, setstudEmail] = useState(" ")
+    const [studMob, setstudMob] = useState("")
+    const [stidGender, setstidGender] = useState("")
+    const [studCourse, setstudCourse] = useState("")
 
     const handleForm = (event) => {
         event.preventDefault()
         const formData = new FormData(event.target)
         const studemt = Object.fromEntries(formData.entries())
         console.log("DATA", studemt)
+        ressetForm()
     }
+
+    function ressetForm() {
+        setstudName("")
+        setstudEmail("")
+        setstudMob("")
+    }
+
     return (
         <div>
             <h4>Student Information</h4>
             <form onSubmit={handleForm}>
                 <div>
                     <label>Student Name</label>
-                    <input type="text" name="sname" id="" />
+                    <input type="text"
+                        value={studName}
+                        onChange={(e) => setstudName(e.target.value)}
+                        name="sname" id="" />
                 </div>
                 <div>
                     <label>Student Email</label>
-                    <input type="email" name="semail" id="" />
+                    <input type="email"
+                        value={studEmail}
+                        onChange={(e) => setstudEmail(e.target.value)}
+                        name="semail" id="" />
                 </div>
                 <div>
                     <label>Student Mobile</label>
-                    <input type="tel" name="smobno" id="" />
+                    <input type="tel"
+                        value={studMob}
+                        onChange={(e) => setstudMob(e.target.value)}
+                        name="smobno" id="" />
                 </div>
 
                 <div>
                     <label>Student Gender</label>
-                    <input type="radio" name="sgender" id="m" value="Male" />
+                    <input type="radio" name="sgender" onChange={(e) => setstidGender(e.target.value)} id="m" value="Male" />
                     <label htmlFor="m">Male</label>
 
-                    <input type="radio" name="sgender" id="f" value="Female" />
+                    <input type="radio" name="sgender"
+                        onChange={(e) => setstidGender(e.target.value)}
+                        id="f" value="Female" />
                     <label htmlFor="f">Female</label>
                 </div>
                 <div>
                     <label>Select Course</label>
-                    <select name="scourse" id="">
+                    <select onChange={(e) => setstudCourse(e.target.value)} name="scourse" id="">
                         <option>Select</option>
                         <option value="MCA">MCA</option>
                         <option value="BCA">BCA</option>
